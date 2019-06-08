ff29da20fe99041eddf83787047df7f8ce82f271
Cloudmersive <support@cloudmersive.com>
2019-06-08 01:42:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ Apikey.apiKey = '6639dfa5-7973-488f-a551-291207ecb6e6';
 
 var apiInstance = new CloudmersiveConvertApiClient.MergeDocumentApi();
 
-var inputFile1 = Buffer.from(fs.readFileSync("C:\\temp\\inputfile").buffer); // File | First input file to perform the operation on.
+var inputFile1 = Buffer.from(fs.readFileSync("spreadsheet1.xlsx").buffer); // File | First input file to perform the operation on.
 
-var inputFile2 = Buffer.from(fs.readFileSync("C:\\temp\\inputfile").buffer); // File | Second input file to perform the operation on (more than 2 can be supplied).
+var inputFile2 = Buffer.from(fs.readFileSync("spreadsheet2.xlsx").buffer); // File | Second input file to perform the operation on (more than 2 can be supplied).
 
 
 var callback = function(error, data, response) {
   if (error) {
     console.error(error);
   } else {
+    fs.writeFile("./output.xlsx", data);
     console.log('API called successfully. Returned data: ' + data);
   }
 };
