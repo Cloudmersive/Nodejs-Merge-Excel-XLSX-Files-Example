dabe31222f59ca0822effff0dd46afebabc5a4be
Cloudmersive <support@cloudmersive.com>
2019-06-08 01:11:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello World");
+
+var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = '6639dfa5-7973-488f-a551-291207ecb6e6';
+
+
+
+var apiInstance = new CloudmersiveConvertApiClient.MergeDocumentApi();
+
+var inputFile1 = Buffer.from(fs.readFileSync("C:\\temp\\inputfile").buffer); // File | First input file to perform the operation on.
+
+var inputFile2 = Buffer.from(fs.readFileSync("C:\\temp\\inputfile").buffer); // File | Second input file to perform the operation on (more than 2 can be supplied).
+
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.mergeDocumentXlsx(inputFile1, inputFile2, callback);
